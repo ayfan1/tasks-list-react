fad498e5f8a6b0a181ecddd33bb05a94e9f0b7fd
Ayfan Asma <ivan.ismailov2000@gmail.com>
2022-11-27 23:39:45 +0100

    toggle task done  interaction

--- src/App.js
+++ src/App.js
@@ -21,6 +21,16 @@ function App() {
     setTasks(tasks => tasks.filter(task => task.id !== id));
   };
 
+  const toggleTaskDone = (id) => {
+    setTasks(tasks => tasks.map(task => {
+      if (task.id === id) {
+        return {...task, done: !task.done};
+      }
+
+      return task;
+    }))
+  };
+
   return (
     <Container>
         <Header title="Tasks list" />
@@ -32,7 +42,12 @@ function App() {
         <Section 
           title="Tasks list"
           body={
-          <Tasks tasks={tasks} hideDone={hideDone} removeTask={removeTask} />
+          <Tasks 
+            tasks={tasks}
+            hideDone={hideDone}
+            removeTask={removeTask}
+            toggleTaskDone={toggleTaskDone} 
+          />
         }
           extraHeaderContent={
           <Buttons 
