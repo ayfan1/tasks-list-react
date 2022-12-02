1d9d67d7504bca457513dd705e86a47419604bb9
Ayfan Asma <ivan.ismailov2000@gmail.com>
2022-12-02 23:42:48 +0100

    add  task id

--- src/Tasks/index.js
+++ src/Tasks/index.js
@@ -20,7 +20,7 @@ const Tasks = ({tasks, hideDone, removeTask, toggleTaskDone}) => (
         <span className={
           `tasks__content${task.done ? " tasks__content--done" : ""}`
         }>
-          {task.content}
+          {task.id} - {task.content}
         </span>
         <button
           className="tasks__button tasks__button--remove"
